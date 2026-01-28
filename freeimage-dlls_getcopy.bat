f293b53bcc46e74e260082d99ebc191fe183a91b
docte <docte@ASUSDROIDE>
2026-01-28 10:27:05 -0500

    Add project files.

---
+++ freeimage-dlls_getcopy.bat
@@ -0,0 +1,21 @@
+rem 32bit
+rem 32bit
+rem 32bit
+
+rem debug
+copy ..\lib-src\freeimage_vs2026\Dist\x32\FreeImage.dll .\FreeImage.dll
+copy ..\lib-src\freeimage_vs2026\Dist\x32\FreeImage.dll .\debug\FreeImage.dll
+rem release
+copy ..\lib-src\freeimage_vs2026\Dist\x32\FreeImage.dll .\release\FreeImage.dll
+
+rem 64bit
+rem 64bit
+rem 64bit
+
+rem debug
+rem copy ..\lib-src\freeimage_vs2026(x64)\Dist\x64\FreeImage.dll .\FreeImage.dll
+rem copy ..\lib-src\freeimage_vs2026(x64)\Dist\x64\FreeImage.dll .\x64\debug\FreeImage.dll
+rem release
+rem copy ..\lib-src\freeimage_vs2026(x64)\Dist\x64\FreeImage.dll .\x64\release\FreeImage.dll
+
+
---
+++ spiwavsetlib-dlls_getcopy.bat
@@ -0,0 +1,4 @@
+copy ..\spiwavsetlib_vs2026u\Release\spiwavsetlib_vs2019.dll .\spiwavsetlib_vs2019.dll
+copy ..\spiwavsetlib_vs2026u\Release\spiwavsetlib_vs2019.dll .\debug\spiwavsetlib_vs2019.dll
+copy ..\spiwavsetlib_vs2026u\Release\spiwavsetlib_vs2019.dll .\release\spiwavsetlib_vs2019.dll
+
